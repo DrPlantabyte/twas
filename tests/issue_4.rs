b7a5c0636224cad14d8114244b533479834f9e6a
Chris Hall <explosivegnome@yahoo.com>
2025-08-19 12:42:39 +0800

    replicated the issue

---
+++ tests/issue_4.rs
@@ -0,0 +1,54 @@
+use rand::Rng;
+use twas;
+
+const CULTURE_BY_REGION_YAML: &str = r#"
+any:
+  - iltanno
+"#;
+
+const SPECIES_BY_CULTURE_YAML: &str = r#"
+iltanno:
+  human: 70
+"#;
+
+const GENDER_BY_SPECIES_YAML: &str = r#"
+# standard, with ~4.5% rate of nonbinary identification
+default:
+  female: 10
+  male: 10
+  nonbinary: 1
+human:
+  - "${gender-by-species/default}"
+"#;
+
+const NAME_BY_CULTURE_YAML: &str = r#"
+iltanno:
+  female:
+    - "${names/iltanno/female} ${names/iltanno/family}"
+"#;
+
+const NAMES_ILTANNO_CSV: &str = r#"family,female
+Campos,Lisa
+"#;
+
+fn load_data(seed: u64) -> twas::Interpreter<impl Rng> {
+	let mut interp = twas::Interpreter::from_seed(seed);
+	interp.load_yaml_str("culture-by-region", CULTURE_BY_REGION_YAML);
+	interp.load_yaml_str("species-by-culture", SPECIES_BY_CULTURE_YAML);
+	interp.load_yaml_str("gender-by-species", GENDER_BY_SPECIES_YAML);
+	interp.load_yaml_str("name-by-culture", NAME_BY_CULTURE_YAML);
+	interp.load_csv_str("names/iltanno", NAMES_ILTANNO_CSV);
+	interp
+}
+
+#[test]
+fn test_nested_reference() {
+	let mut interpreter = load_data(12345);
+	let test_str = r#"${{id: culture-by-region/any, ref: culture, hidden: true}}${{id: "species-by-culture/$culture", ref: species, hidden: true}}${{id: "gender-by-species/$species", ref: gender, hidden: true}}${{id: "name-by-culture/$culture/$gender"}} (${@gender} ${{id: "@culture", case: title}} ${{id: "@species", case: title}})"#;
+	eprintln!("test_str: {}", test_str);
+	let expected_output = "Lisa Campos (female Iltanno Human)";
+	eprintln!("expected_output: {}", expected_output);
+	let actual_output = interpreter.eval(test_str).unwrap();
+	eprintln!("actual_output: {}", actual_output);
+	assert_eq!(expected_output, actual_output);
+}
